1fcd6caeee15a5a6bcd5cf4a951e515a609fb4b7
James Claydon <james.claydon@paysafe.com>
2018-03-12 15:39:59 +0000

    boiler plate manipulation

--- routes/admin.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET Admin page */
-router.get('/', function(req, res, next) {
-  res.render('admin');
-});
-
-module.exports = router;
