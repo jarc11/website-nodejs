3d21ab24a6238303415c737945ab12645c84086e
James Claydon <james.claydon@paysafe.com>
2018-03-13 14:44:42 +0000

    Bulma and a start

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Sample App' });
 });
 
 router.get('/admin', function(req, res, next) {
